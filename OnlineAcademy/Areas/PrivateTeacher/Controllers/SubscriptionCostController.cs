f69fdaf3608f5b6ea8042b22c2ef8d088c9f04c3
basmawebas <basma_wemalak@yahoo.com>
2020-11-13 12:23:42 +0200

    Prefered design and the same will be commited

---
+++ OnlineAcademy/Areas/PrivateTeacher/Controllers/SubscriptionCostController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineAcademy.Areas.PrivateTeacher.Controllers
+{
+    public class SubscriptionCostController : Controller
+    {
+        // GET: PrivateTeacher/SubscriptionCost
+        public ActionResult Index()
+        {
+            return View();
+        } 
+        public ActionResult ShowSubscriptionTypes()
+        {
+            return View();
+        }
+    }
+}
